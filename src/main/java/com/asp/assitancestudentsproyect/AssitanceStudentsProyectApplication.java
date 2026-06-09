8e1dca76b096d6bda111865ef7ed5d350e639d7b
Tico <aronticona5@gmail.com>
2026-06-09 18:23:56 -0500

    Primer commit

---
+++ src/main/java/com/asp/assitancestudentsproyect/AssitanceStudentsProyectApplication.java
@@ -0,0 +1,13 @@
+package com.asp.assitancestudentsproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssitanceStudentsProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssitanceStudentsProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asp/assitancestudentsproyect/AssitanceStudentsProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asp.assitancestudentsproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssitanceStudentsProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
